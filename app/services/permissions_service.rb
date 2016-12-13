762ab443a081972cbde8d96619cf236154f7c99c
Matt D <MDes41@users.noreply.github.com>
2016-12-13 11:57:48 -0700

    added user permission

--- app/services/permissions_service.rb
+++ app/services/permissions_service.rb
@@ -6,16 +6,44 @@ class PermissionsService
 	end
 
 	def allow?
-			return true if controller == "stores" && action.in?(%w(index))
-			return true if controller == "items" && action.in?(%w(show index))
-			return true if controller == "sessions" && action.in?(%w(new create destroy))
-		if user.registered?
-			return true if controller == "stores" && action.in?(%w(show index))
-			return true if controller == "items" && action.in?(%w(show index))
-			return true if controller == "sessions" && action.in?(%w(new create destroy))
+		if user.admin?
+			admin_permissions
+		elsif user.store_admin?
+			store_admin_permissions
+		elsif user.registered?
+			registered_user_permissions
+		else
+			guest_user_permissions
 		end
 	end
 
 	private
 		attr_reader :user, :controller, :action
+
+		def admin_permissions
+			return true if controller == "stores" && action.in?(%w(show index))
+			return true if controller == "items" && action.in?(%w(show index))
+			return true if controller == "sessions" && action.in?(%w(new create destroy))
+			return true if controller == "orders" && action.in?(%w(index show))
+			return true if controller == "users" && action.in?(%w(index show))
+		end
+
+		def store_admin_permissions
+			return true if controller == "stores" && action.in?(%w(show index))
+			return true if controller == "items" && action.in?(%w(show index))
+			return true if controller == "sessions" && action.in?(%w(new create destroy))
+			return true if controller == "orders" && action.in?(%w(index show))
+		end
+
+		def registered_user_permissions
+			return true if controller == "stores" && action.in?(%w(show index))
+			return true if controller == "items" && action.in?(%w(show index))
+			return true if controller == "sessions" && action.in?(%w(new create destroy))
+		end
+
+		def guest_user_permissons
+			return true if controller == "stores" && action.in?(%w(index))
+			return true if controller == "items" && action.in?(%w(show index))
+			return true if controller == "sessions" && action.in?(%w(new create destroy))
+		end
 end
